ded0ffe54f9ce73d6bd5a9802b32ccf8a7db8677
qianxinglim <64999593+qianxinglim@users.noreply.github.com>
2021-11-08 02:09:17 +0800

    env chg l

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, 'public');
+// app.use(express.static(publicPath));
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -38,29 +38,27 @@ app.use('/auth', authRouteUrls);
 app.use('/post', postRouteUrls);
 app.use('/category', catRouteUrls);
 
-// if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging') {
-//     // Express will serve up production assets i.e. main.js
-//     const path = require('path');
-//     const publicPath = path.join(__dirname, '/build');
-//     app.use(express.static(publicPath));
-//     // If Express doesn't recognize route serve index.html
-//     //const path = require('path');
-//     app.get('*', (req, res) => {
-//        res.sendFile(
-//            path.join(publicPath)
-//        );
-//     });
-// }
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging') {
+    // Express will serve up production assets i.e. main.js
+    const path = require('path');
+    const publicPath = path.join(__dirname + 'build');
+    app.use(express.static(publicPath));
+    // If Express doesn't recognize route serve index.html
+    //const path = require('path');
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(publicPath, 'index.html'));
+    });
+}
 
 // app.get('*', (req,res) =>{
 //     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 //   });
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(publicPath, 'index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
